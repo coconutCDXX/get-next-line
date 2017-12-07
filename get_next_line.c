48b529f38d8405b0e33b3a3c1bcc941dab2e0394
Cora wartelle <cwartell@e1z1r4p4.42.us.org>
2017-12-06 22:09:25 -0800

    no error finally yaygit statusgit status

--- get_next_line.c
+++ get_next_line.c
@@ -6,36 +6,36 @@
 /*   By: cwartell <cwartell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 19:14:44 by cwartell          #+#    #+#             */
-/*   Updated: 2017/11/29 23:42:49 by cwartell         ###   ########.fr       */
+/*   Updated: 2017/12/06 22:08:40 by cwartell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-//#include "libft/libft.h"
 
 int		get_next_line(const int fd, char **line)
 {
-	char buffer;
+	char	buffer;
 	char	*tmp;
-	int i;
-	int j;
+	int		i;
+	int		j;
 
 	i = 0;
-	//printf("here we go again\n");
-	// if (**line != '\0')
-	// 	free(*line);
-	*line = (char*)malloc(1 * sizeof(char) + 1);
-	tmp = (char*)malloc(sizeof(char) * BUFF_SIZE);
-	while ((j = read(fd, &buffer, 1)) > 0 && buffer != '\n')
+	if (fd < 0 || (read(fd, &buffer, 0) < 0))
+		return (-1);
+	// if (*line != NULL)
+	// 	free(line[0]);
+	tmp = (char*)malloc(sizeof(char) * 100000);
+	while ((j = read(fd, &buffer, BUFF_SIZE)) > 0 && buffer != '\n')
 	{
 		i++;
-		*line = (char*)malloc(i * sizeof(char) + 1);
 		tmp[i - 1] = buffer;
-		strcpy(*line, tmp);
 	}
-	if (i == 0)
-		return 0;
+	*line = (char*)malloc(i * sizeof(char) + 1);
+	ft_strcpy(*line, tmp);
+	//free(tmp);
 	if (j == -1)
-		return -1;
+		return (-1);
+	if (j == 0 && i == 0)
+		return (0);
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,11 +1,14 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 1
 
 int		get_next_line(int fd, char **line);
 
